bdb3c4f1812780e5281d8e8816f615a67d3715a9
Joe Flowers <jflowers45@gmail.com>
2015-07-07 19:16:10 -0400

    epub3 export fix

--- includes/modules/export/epub3/class-pb-epub3.php
+++ includes/modules/export/epub3/class-pb-epub3.php
@@ -233,7 +233,7 @@ class Epub3 extends Epub\Epub201 {
 		$path_to_epub_assets = $this->tmpDir . '/OEBPS/assets';
 
 		$css = file_get_contents( $path_to_copy_of_stylesheet );
-		$css = static::injectHouseStyles( $css );
+		$css = static::injectHouseStyles( $css, $metadata );
 
 		// Search for url("*"), url('*'), and url(*)
 		$url_regex = '/url\(([\s])?([\"|\'])?(.*?)([\"|\'])?([\s])?\)/i';
