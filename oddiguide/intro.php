910a751d329cd78991e4e69dea9d97cdecdd8348
Joe Flowers <jflowers45@gmail.com>
2015-08-24 13:08:16 -0400

    getting started guide

---
+++ oddiguide/intro.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<title>Getting Started with ODDIBooks</title>
+		<style>
+			h1 {
+				color:red;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>RECOMMENDED RESOURCES</h1>
+		<h2><a href="http://guide.pressbooks.com/chapter/the-short-5-step-guide-to-using-pressbooks/">The 5 Step Guide to Using Pressbooks</a></h2>
+		Quick and easy getting started guide.  Please ignore step 1 since you’ve already been provided an account.
+
+		<h2><a href="http://guide.pressbooks.com/">Pressbooks Online Manual</a></h2>
+		The 5-step guide mentioned above is part of a much bigger pressbooks online manual.  Click "read" then click "table of contents" to see all the different topics available.
+
+		<h2><a href="https://www.youtube.com/user/pressbooks">Pressbooks YouTube Channel</a></h2>
+		The Pressbooks YouTube channel has a number of videos you may find helpful.
+
+		<h2>Specific YouTube Videos</h2>
+
+		<div class="specificVid">
+			<a href="https://www.youtube.com/watch?v=Vf6uF5zEGCE">Rearranging Parts and Chapters in Pressbooks</a>
+		</div>
+
+		<div class="specificVid">
+			<a href="https://www.youtube.com/watch?v=r7aAOuYLSs0">Editing Your Book's Metadata in Pressbooks</a>
+		</div>
+
+		<div class="specificVid">
+			<a href="https://www.youtube.com/watch?v=HN92hLgGzek">How to Export Your Finished Book from Pressbooks</a>
+		</div>
+	</body>
+</html>
