8cf8bc1044ab0ea45adaf078ff7feda01f4b041e
Joe Flowers <jflowers45@gmail.com>
2015-09-14 13:14:38 -0400

    update getting started guide

--- oddiguide/about.php
+++ oddiguide/about.php
@@ -1,28 +1,47 @@
 <?php include('header.php'); ?>
 	
-	<h2>Recommended Resources</h2>
-	<h2><a href="http://guide.pressbooks.com/chapter/the-short-5-step-guide-to-using-pressbooks/">The 5 Step Guide to Using Pressbooks</a></h2>
-	Quick and easy getting started guide.  Please ignore step 1 since you've already been provided an account.
+<style>
 
-	<h2><a href="http://guide.pressbooks.com/">Pressbooks Online Manual</a></h2>
-	The 5-step guide mentioned above is part of a much bigger pressbooks online manual.  Click "read" then click "table of contents" to see all the different topics available.
+	ul {
+		padding-bottom:5px;
+	}
 
-	<h2><a href="https://www.youtube.com/user/pressbooks">Pressbooks YouTube Channel</a></h2>
-	The Pressbooks YouTube channel has a number of videos you may find helpful.
+	li.question {
+	  font-weight:bold;
+	  font-size:1.3em;
+	  padding-bottom:5px;
+	}
+	li.answer {
+	 list-style: none;
+	 padding-bottom: 5px;
+	}
 
-	<BR><BR>
+</style>
+
+<h2>Pressbooks Resources</h2>
+<ul>
+	<li class="question"><a href="http://guide.pressbooks.com/chapter/the-short-5-step-guide-to-using-pressbooks/">The 5 Step Guide to Using Pressbooks</a></li>
+	<li class="answer">Quick and easy getting started guide.  Please ignore step 1 since you've already been provided an account.</li>
+</ul>
+<ul>
+	<li class="question"><a href="http://guide.pressbooks.com/">Pressbooks Online Manual</a></li>
+	<li class="answer">The 5-step guide mentioned above is part of a much bigger pressbooks online manual.  Click "read" then click "table of contents" to see all the different topics available.</li>
+</ul>
+<ul>
+	<li class="question"><a href="https://www.youtube.com/user/pressbooks">Pressbooks YouTube Channel</a></li>
+	<li class="answer">The Pressbooks YouTube channel has a number of videos you may find helpful.</li>
+</ul>
+<ul>
+	<li class="question"><a href="http://guide.pressbooks.com/chapter/the-short-5-step-guide-to-using-pressbooks/">The 5 Step Guide to Using Pressbooks</a></li>
+	<li class="answer">Pressbooks automatically generates one or more table of contents for each format.</li>
+</ul>
+
 	<h2>Specific YouTube Videos</h2>
 
-	<div class="specificVid">
-		<a href="https://www.youtube.com/watch?v=Vf6uF5zEGCE">Rearranging Parts and Chapters in Pressbooks</a>
-	</div>
-
-	<div class="specificVid">
-		<a href="https://www.youtube.com/watch?v=r7aAOuYLSs0">Editing Your Book's Metadata in Pressbooks</a>
-	</div>
-
-	<div class="specificVid">
-		<a href="https://www.youtube.com/watch?v=HN92hLgGzek">How to Export Your Finished Book from Pressbooks</a>
-	</div>
-
+	<ul>
+		<li><a href="https://www.youtube.com/watch?v=Vf6uF5zEGCE">Rearranging Parts and Chapters in Pressbooks</a></li>
+		<li><a href="https://www.youtube.com/watch?v=r7aAOuYLSs0">Editing Your Book's Metadata in Pressbooks</a></li>
+		<li><a href="https://www.youtube.com/watch?v=HN92hLgGzek">How to Export Your Finished Book from Pressbooks</a></li>
+	</ul>
+	
 <?php include('footer.php'); ?>
--- oddiguide/index.php
+++ oddiguide/index.php
@@ -3,7 +3,7 @@
 <style>
 
 	ul {
-		padding-bottom:15px;
+		padding-bottom:10px;
 	}
 
 	li.question {
@@ -13,12 +13,37 @@
 	}
 	li.answer {
 	 list-style: none;
-	 padding-bottom: 15px;
+	 padding-bottom: 10px;
 	}
 
 </style>
 
 	<h2>FAQ</h2>
+	
+	<ul>
+		<li class="question">Which theme should I use?</li>
+		<li class="answer">Pressbooks automatically generates one or more table of contents for each format.</li>
+	</ul>
+
+	<ul>
+		<li class="question">What is each type of user allowed to do?</li>
+		<li class="answer">Pressbooks has roles for Administrator, editor, author, contributor, and subscriber.  Check the <a target="_blank" href="http://guide.pressbooks.com/chapter/users/">USERS</a> section of the pressbooks guide for details on their abilities.</li>
+	</ul>
+
+	<ul>
+		<li class="question">How do I change the styles when editing text?</li>
+		<li class="answer">There are two ways to edit styles to customize the appearance of your book: <BR /><BR />
+			<ul>
+				<li>You can click the <a target='new' href='toolbartoggle.png'>toolbar toggle</a> button when editing text and it will reveal a number of formatting options.</li>
+				<li>You can go to Appearance -> Edit CSS in the left navigation and edit actual CSS rules there.  You must be sure to select either ePub or mPDF as the output.  The <a target='new'  href='http://guide.pressbooks.com/chapter/customizing-your-exports/'>Pressbooks Instructions on Custom CSS</a> may prove useful as well.</li>
+			</ul>
+		</li>
+	</ul>
+	<ul>
+		<li class="question">How do I change the font?</li>
+		<li class="answer">In order to streamline the process of eBook creation, we are only providing a single font for each language.  We have to use open source fonts so that we can legally embed them in the ePubs.</li>
+	</ul>
+
 	<ul>
 		<li class="question">How do I create a Table of Contents (TOC)?</li>
 		<li class="answer">Pressbooks automatically generates one or more table of contents for each format.<BR /><BR />
@@ -26,7 +51,11 @@
 				<li>EPUB MENU SYSTEM TOC: 
 					<ul>
 						<li>ePubs are required to include a TOC that is accessible via the menu system in most e-readers.</li>
-						<li>Pressbooks automatically creates this for you and currently only outputs the first level of navigation.</li>
+						<li>Pressbooks automatically creates this for you and currently only outputs the first level of navigation. 
+							<ul>
+								<li><strong>NOTE: Pressbooks plans to allow second level.  This on their roadmap for q3 2015.</strong></li>
+							</ul>
+						</li>
 					</ul>
 				</li>
 				<li>EPUB EMBEDDED TOC: 
@@ -51,21 +80,6 @@
 			</ul>
 		</li>
 	</ul>
-	<ul>
-		<li class="question">How do I change the styles when editing text?</li>
-		<li class="answer">There are two ways to edit styles to customize the appearance of your book: <BR /><BR />
-			<ul>
-				<li>You can click the <a target='new' href='toolbartoggle.png'>toolbar toggle</a> button when editing text and it will reveal a number of formatting options.</li>
-				<li>You can go to Appearance -> Edit CSS in the left navigation and edit actual CSS rules there.  You must be sure to select either ePub or mPDF as the output.  The <a target='new'  href='http://guide.pressbooks.com/chapter/customizing-your-exports/'>Pressbooks Instructions on Custom CSS</a> may prove useful as well.</li>
-			</ul>
-		</li>
-	</ul>
-	<ul>
-		<li class="question">How do I change the font?</li>
-		<li class="answer">In order to streamline the process of eBook creation, we are only providing a single font for each language.  We have to use open source fonts so that we can embed them in the ePubs.</li>
-	</ul>
-	
-
 	
 
 <?php include('footer.php'); ?>
